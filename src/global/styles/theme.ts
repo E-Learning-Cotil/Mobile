bae27b41a72389c3d7736315cb214fe29bbbc030
Gianni Labella <gianni0labella@gmail.com>
2021-08-16 19:20:01 -0300

    Add global theme
    Co-Authored-By: Matheus Mastelini  <60967298+Amazing512@users.noreply.github.com>

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,26 @@
+export const theme = {
+	colors: {
+		background: '#202020',
+		gray90: '#3D3D3D',
+		gray80: '#505050',
+
+		green100: '#007013',
+		green90: '#009418',
+		green80: '#0CBE29',
+		green70: '#4AED64',
+
+		purple100: '#4E0670',
+		purple90: '#6C1795',
+		purple80: '#9F18DF',
+
+		highlight: '#ABB1CC',
+    white: '#fff'
+
+	},
+
+	fonts: {
+		title400: 'Righteous_400Regular',
+		text400: 'Quicksand_400Regular',
+		text700: 'Quicksand_700Bold',
+	}
+};
