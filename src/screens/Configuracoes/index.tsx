8de51c6fca2697692af08965753302ce932fb613
Gianni Labella <gianni0labella@gmail.com>
2021-09-21 21:23:38 -0300

    Add User Config Screen
    Co-Authored-By: Matheus Mastelini  <60967298+Amazing512@users.noreply.github.com>

--- src/screens/Configuracoes/index.tsx
+++ src/screens/Configuracoes/index.tsx
@@ -1,11 +1,129 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { View } from 'react-native';
+import { View, TextInput, Text, ActivityIndicator, ScrollView } from 'react-native';
 
-export function SignIn(){
-    return(
-      <View>
+import { LabelText } from '../../components/LabelText';
+import { NavBar } from '../../components/NavBar';
+import { CardAtividade } from '../../components/CardAtividade';
+import { CardTurma } from '../../components/CardTurma';
 
-      </View>
-    );
-  }
+import { useAuth } from '../../contexts/auth';
+
+import { theme } from '../../global/styles/theme';
+import { styles } from './styles';
+import api from '../../services/api';
+
+import ContentLoader, { Rect } from "react-content-loader/native"
+import { Avatar } from '../../components/Avatar';
+
+interface InfoAluno {
+	ra: number;
+	nome: string;
+	email: string;
+  idSerie: number;
+}
+
+interface InfoProfessor {
+	rg: number;
+	nome: string;
+	email: string;
+}
+
+
+
+export function Configuracoes(){
+	const { user } = useAuth();
+
+  const [info, setInfo] = useState<InfoAluno | InfoProfessor>();
+	
+  const role = user?.role;
+	const color = role === 'ALUNO' ? theme.colors.green90 : theme.colors.purple90;
+
+  /*useEffect(() => {
+		async function getInfo() {
+        const {	
+          data,
+          status 
+        } = user?.role === 'Aluno' ? await api.get('/Aluno/' + {user?.id}) : await api.get('/pagina-inicial');
+				setInfo(data);	
+    }
+
+		getInfo();
+	}, []);*/
+
+	return(
+		<View style={styles.container}>          
+			<NavBar 
+				title="Configurações" iconName="cog"
+				color={color}
+			/>
+			<ScrollView style={styles.content}>
+        <View style={styles.avatarDiv}>
+          <View style={[
+              styles.avatarBackground,
+              {backgroundColor: color}
+            ]}>
+              
+          </View>
+          <Avatar 
+              urlImage={user?.foto} 
+              imageBorderRadius={{borderRadius: 10}} 
+              style={[
+                {width: 100, height: 100, marginTop: 20, marginLeft: 20, position: 'absolute'}
+              ]}
+            />
+        </View>
+
+        <View style={styles.userInfo}>
+          <LabelText 
+            title="Apelido"  
+            color={color}
+          />
+          <TextInput style={[
+              {},
+              styles.textConfig
+            ]}>
+              {user?.nome}
+          </TextInput>
+
+          <LabelText 
+            title="Senha"  
+            color={color}
+          />
+          <TextInput style={[
+              {},
+              styles.textConfig
+            ]}>
+              {}
+          </TextInput>
+
+          <LabelText 
+            title="Email"  
+            color={color}
+          />
+          <TextInput style={[
+              {},
+              styles.textConfig
+            ]}>
+              {user?.email}
+          </TextInput>
+
+          <LabelText 
+            title="Turma"  
+            color={color}
+          />
+          <Text style={styles.textConfig}>
+              {}
+          </Text>
+
+          <LabelText 
+            title="Tema"  
+            color={color}
+          />
+          <Text style={styles.textConfig}>Funcionalidade ainda não disponivel</Text>
+        </View>
+        
+			</ScrollView>
+		</View>
+	);
+}
--- src/screens/Configuracoes/styles.ts
+++ src/screens/Configuracoes/styles.ts
@@ -3,9 +3,39 @@ import { StyleSheet } from 'react-native';
 import { theme } from '../../global/styles/theme';
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      }
+  container: {
+		flex: 1,
+		paddingTop: 100,
+		backgroundColor: theme.colors.background,
+    //alignItems: 'center',
+	},
+
+  avatarDiv: {
+    height: 120,
+    width: '100%',
+  },
+
+  avatarBackground: {
+     height:100,
+     width: '100%',
+     borderRadius: 10,
+    },
+
+  content: {
+		padding: 20,
+		paddingTop: 20,
+	},
+
+  userInfo: {
+    marginTop: 10,
+  },
+
+  textConfig: {
+    color: theme.colors.white,
+    fontFamily: theme.fonts.text400,
+    fontSize: 20,
+  },
+
+
+
 });
