21107b5bb9cafc0cdf096e62e63af89177df5628
Gianni Labella <gianni0labella@gmail.com>
2021-09-28 19:10:24 -0300

    Update Config Screen

--- src/screens/Configuracoes/index.tsx
+++ src/screens/Configuracoes/index.tsx
@@ -1,43 +1,58 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from "react";
 
-import { View, TextInput, Text, ActivityIndicator, ScrollView } from 'react-native';
+import {
+  View,
+  TextInput,
+  Text,
+  ActivityIndicator,
+  ScrollView,
+} from "react-native";
 
-import { LabelText } from '../../components/LabelText';
-import { NavBar } from '../../components/NavBar';
-import { CardAtividade } from '../../components/CardAtividade';
-import { CardTurma } from '../../components/CardTurma';
+import { LabelText } from "../../components/LabelText";
+import { NavBar } from "../../components/NavBar";
+import { CardAtividade } from "../../components/CardAtividade";
+import { CardTurma } from "../../components/CardTurma";
 
-import { useAuth } from '../../contexts/auth';
+import { useAuth } from "../../contexts/auth";
 
-import { theme } from '../../global/styles/theme';
-import { styles } from './styles';
-import api from '../../services/api';
+import { theme } from "../../global/styles/theme";
+import { styles } from "./styles";
+import api from "../../services/api";
 
-import ContentLoader, { Rect } from "react-content-loader/native"
-import { Avatar } from '../../components/Avatar';
+import ContentLoader, { Rect } from "react-content-loader/native";
+import { Avatar } from "../../components/Avatar";
 
 interface InfoAluno {
-	ra: number;
-	nome: string;
-	email: string;
-  idSerie: number;
-}
-
-interface InfoProfessor {
-	rg: number;
-	nome: string;
-	email: string;
+  nomeTurma: string;
 }
 
 
+export function Configuracoes() {
+  const { user } = useAuth();
 
-export function Configuracoes(){
-	const { user } = useAuth();
+  const [info, setInfo] = useState<InfoAluno>();
 
-  const [info, setInfo] = useState<InfoAluno | InfoProfessor>();
-	
   const role = user?.role;
-	const color = role === 'ALUNO' ? theme.colors.green90 : theme.colors.purple90;
+  const color = role === "ALUNO" ? theme.colors.green90 : theme.colors.purple90;
+
+  const [ loading, setLoading ] = useState(true);
+
+	useEffect(() => {
+		async function getInfoAluno() {
+			if (role === 'ALUNO') {
+				const {	
+					data,
+					status 
+				} = await api.get('/turmas/'); 
+
+				setInfo(data);
+			}
+			
+			setLoading(false);
+		}
+
+		getInfoAluno();
+	}, []);
 
   /*useEffect(() => {
 		async function getInfo() {
@@ -51,79 +66,50 @@ export function Configuracoes(){
 		getInfo();
 	}, []);*/
 
-	return(
-		<View style={styles.container}>          
-			<NavBar 
-				title="Configurações" iconName="cog"
-				color={color}
-			/>
-			<ScrollView style={styles.content}>
+  return (
+    <View style={styles.container}>
+      <NavBar title="Configurações" iconName="cog" color={color} />
+      <ScrollView style={styles.content}>
         <View style={styles.avatarDiv}>
-          <View style={[
-              styles.avatarBackground,
-              {backgroundColor: color}
-            ]}>
-              
-          </View>
-          <Avatar 
-              urlImage={user?.foto} 
-              imageBorderRadius={{borderRadius: 10}} 
-              style={[
-                {width: 100, height: 100, marginTop: 20, marginLeft: 20, position: 'absolute'}
-              ]}
-            />
+          <View
+            style={[styles.avatarBackground, { backgroundColor: color }]}
+          ></View>
+          <Avatar
+            urlImage={user?.foto}
+            imageBorderRadius={{ borderRadius: 10 }}
+            style={[
+              {
+                width: 100,
+                height: 100,
+                marginTop: 20,
+                marginLeft: 20,
+                position: "absolute",
+              },
+            ]}
+          />
         </View>
 
         <View style={styles.userInfo}>
-          <LabelText 
-            title="Apelido"  
-            color={color}
-          />
-          <TextInput style={[
-              {},
-              styles.textConfig
-            ]}>
-              {user?.nome}
-          </TextInput>
+          <LabelText title="Apelido" color={color} />
+          <TextInput style={styles.textConfig}>{user?.nome}</TextInput>
 
-          <LabelText 
-            title="Senha"  
-            color={color}
-          />
-          <TextInput style={[
-              {},
-              styles.textConfig
-            ]}>
-              {}
-          </TextInput>
+          <LabelText title="Email" color={color} />
+          <TextInput style={styles.textConfig}>{user?.email}</TextInput>
 
-          <LabelText 
-            title="Email"  
-            color={color}
-          />
-          <TextInput style={[
-              {},
-              styles.textConfig
-            ]}>
-              {user?.email}
-          </TextInput>
+          <LabelText title="Senha" color={color} />
+          <TextInput style={styles.textConfig}>{"**********"}</TextInput>
 
-          <LabelText 
-            title="Turma"  
-            color={color}
-          />
-          <Text style={styles.textConfig}>
-              {}
-          </Text>
+          {/*role === 'ALUNO'&& 
+          <View>
+            <LabelText title="Serie" color={color} />
+            <Text style={styles.textConfig}>{info?.serie</Text>
+          </View>*/}
+          
 
-          <LabelText 
-            title="Tema"  
-            color={color}
-          />
-          <Text style={styles.textConfig}>Funcionalidade ainda não disponivel</Text>
+          <LabelText title="Tema" color={color} />
+          <Text style={styles.textConfig}>Em breve</Text>
         </View>
-        
-			</ScrollView>
-		</View>
-	);
+      </ScrollView>
+    </View>
+  );
 }
