9d50c8ccc0c60cd02564b125a62887b5aa579dd4
Gianni Labella <gianni0labella@gmail.com>
2021-10-17 00:53:55 -0300

    Update Conversa Screen - Add Message Component

--- src/screens/Conversa/index.tsx
+++ src/screens/Conversa/index.tsx
@@ -3,23 +3,31 @@ import React, { useEffect, useState } from 'react';
 import { View, ScrollView, Text } from 'react-native';
 
 import { NavBar } from '../../components/NavBar';
+import { Message } from '../../components/Message'
 
 import { useAuth } from '../../contexts/auth';
 
 import { theme } from '../../global/styles/theme';
 import { styles } from './styles';
 
-import { Socket } from "socket.io-client";
+import { io } from "socket.io-client";
 
 interface Props {
 	id: string;
-	socket: Socket;
+	name: string;
 }
 
-export function Conversa({ id, socket }: Props) {
+export function Conversa({route: {params: { id, name }}}: Props) {
+	// const {id, name}: Props = route.params;
+
+	console.log("Conversa", id, name)
 	const { user, token } = useAuth();
 	const role = user?.role;
 	const color = role === 'ALUNO' ? theme.colors.green90 : theme.colors.purple90;
+
+	const socket = io("https://elearning-tcc.herokuapp.com");
+
+	const [messages, setMessages] = useState([]);
 
 	useEffect(() => {
 
@@ -28,12 +36,24 @@ export function Conversa({ id, socket }: Props) {
 	return(
 		<View style={styles.container}>          
 			<NavBar 
-				title="Conversas" iconName="comment-alt"
+				title={name} iconName="arrow-left"
 				color={color}
 			/>
 
 			<ScrollView style={styles.content}>
-				<Text>AAAAAAAAAAAAAAAAA</Text>
+				<View style={styles.messages}>
+					{
+						[...Array(6)].map((value, index) => {
+							return <Message key={index} loading={false} message={'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium'} date={'2021-10-17T16:24:21.927Z'} right={index%2 ? true : false} />
+						})
+					}
+
+					{
+						[...Array(6)].map((value, index) => {
+							return <Message key={index} loading={true} right={index%2 ? true : false} />
+						})
+					}
+				</View>
 			</ScrollView>
 		</View>
 	);
--- src/screens/Conversa/styles.ts
+++ src/screens/Conversa/styles.ts
@@ -10,6 +10,9 @@ export const styles = StyleSheet.create({
 	},
 
 	content: {
+	},
+
+	messages: {
 		padding: 20,
 	},
 });
