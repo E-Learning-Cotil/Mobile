5cdd30db2336e7cecc63fa8ba407427df04e55d7
Gianni Labella <gianni0labella@gmail.com>
2021-10-18 19:26:01 -0300

    Update Conversa Screen - Add Dynamic Messages Load
    Co-Authored-By: Matheus Mastelini  <60967298+Amazing512@users.noreply.github.com>

--- src/screens/Conversa/index.tsx
+++ src/screens/Conversa/index.tsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 
-import { View, ScrollView, Text } from 'react-native';
+import { View, ScrollView } from 'react-native';
 
 import { NavBar } from '../../components/NavBar';
 import { Message } from '../../components/Message'
@@ -13,25 +13,59 @@ import { styles } from './styles';
 import { io } from "socket.io-client";
 
 interface Props {
-	id: string;
-	name: string;
+	route: {
+		params: {
+			id: string;
+			name: string;
+		};
+	};
 }
 
-export function Conversa({route: {params: { id, name }}}: Props) {
-	// const {id, name}: Props = route.params;
+interface Message {
+	data: string;
+	mensagem: string;
+	origem: {
+		role: 'ALUNO' | 'PROFESSOR';
+		nome: string;
+		identity: string;
+	}
+}
 
-	console.log("Conversa", id, name)
+export function Conversa({route: {params: { id, name } }}: Props) {
 	const { user, token } = useAuth();
 	const role = user?.role;
 	const color = role === 'ALUNO' ? theme.colors.green90 : theme.colors.purple90;
 
 	const socket = io("https://elearning-tcc.herokuapp.com");
 
-	const [messages, setMessages] = useState([]);
+	const scrollViewRef = useRef<ScrollView | null>(null);
+
+	const [ messages, setMessages ] = useState<Message[]>([]);
+
+	const [ loading, setLoading ] = useState<boolean>();
 
 	useEffect(() => {
+		if (!loading) setLoading(true);
+		if (messages.length !== 0) setMessages([]);
 
-	}, []);
+		socket.on("previous_messages", setMessages);
+		socket.emit("identify", { token });
+		socket.emit("open_chat", { otherUser: id, token });
+		socket.on("new_message", ([data]: Message[]) => {
+			setMessages((prevMessages: Message[]) => {
+                if((data.origem.identity === id)){
+                    return [...prevMessages, data]
+                }
+				else {
+					return prevMessages;
+				}
+            });
+        });
+	}, [id]);
+
+	useEffect(() => {
+		if (messages.length !== 0) setLoading(false);
+	}, [messages]);
 
 	return(
 		<View style={styles.container}>          
@@ -40,15 +74,19 @@ export function Conversa({route: {params: { id, name }}}: Props) {
 				color={color}
 			/>
 
-			<ScrollView style={styles.content}>
+			<ScrollView style={styles.content} ref={scrollViewRef} onContentSizeChange={() => {scrollViewRef.current?.scrollToEnd({ animated: true });}}>
 				<View style={styles.messages}>
 					{
-						[...Array(6)].map((value, index) => {
-							return <Message key={index} loading={false} message={'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium'} date={'2021-10-17T16:24:21.927Z'} right={index%2 ? true : false} />
-						})
-					}
+						!loading
+						?
+						messages.map((message, index) => {
+							const mensagem = message.mensagem;
+							const data = message.data;
+							const right = message.origem.role === role ? true : false;
 
-					{
+							return <Message key={index} loading={false} message={mensagem} date={data} right={right} />
+						})
+						:
 						[...Array(6)].map((value, index) => {
 							return <Message key={index} loading={true} right={index%2 ? true : false} />
 						})
