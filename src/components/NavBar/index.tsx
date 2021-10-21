7d5ff337a6bb7a80ef3108661f7cd14e273ea9fe
Gianni Labella <gianni0labella@gmail.com>
2021-10-21 17:49:43 -0300

    Update NavBar Component - Remove Drawer Button Feedback & Prettified File

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 
-import { View, ViewProps, Image } from 'react-native';
+import { View, TouchableWithoutFeedback } from 'react-native';
 import { LabelIcon } from '../LabelIcon';
 
 import { styles } from './styles';
 
-import { FontAwesome5 } from '@expo/vector-icons'; 
-import { RectButton } from 'react-native-gesture-handler';
+import { FontAwesome5 } from '@expo/vector-icons';
 
-import { DrawerActions, useNavigation, NavigationProp } from '@react-navigation/native';
+import { DrawerActions, useNavigation } from '@react-navigation/native';
 
 type Props = {
 	title?: string;
@@ -16,22 +15,23 @@ type Props = {
 	color?: string | undefined;
 }
 
-export function NavBar({title, iconName, color}: Props){
+export function NavBar({ title, iconName, color }: Props) {
 	const navigation = useNavigation();
-	
-	function openDrawerClickHandler () {
+
+	function openDrawerClickHandler() {
 		navigation.dispatch(DrawerActions.openDrawer());
 	}
 
-    return(
-      <View style={[styles.container, { backgroundColor: color }]}>
+	return (
+		<View style={[styles.container, { backgroundColor: color }]}>
 
-          <LabelIcon title={title} iconName={iconName}/>
-          
-          <RectButton style={styles.navButton} onPress={openDrawerClickHandler}>
-              <FontAwesome5 name="bars" size={32} color="white" />
-          </RectButton>
+			<LabelIcon title={title} iconName={iconName} />
 
-      </View>
-    );
+			<TouchableWithoutFeedback onPress={openDrawerClickHandler}>
+				<View style={styles.navButton}>
+					<FontAwesome5 name="bars" size={32} color="white" />
+				</View>
+			</TouchableWithoutFeedback>
+		</View>
+	);
 }
