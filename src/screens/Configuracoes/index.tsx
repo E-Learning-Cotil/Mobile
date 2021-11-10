385d862dd31fde8c089aef5999a6f0920c421a94
Gianni Labella <gianni0labella@gmail.com>
2021-11-09 21:45:58 -0300

    Update Configuracoes Component
    Co-Authored-By: Matheus Mastelini  <60967298+Amazing512@users.noreply.github.com>

--- src/screens/Configuracoes/index.tsx
+++ src/screens/Configuracoes/index.tsx
@@ -12,6 +12,7 @@ import { LabelText } from "../../components/LabelText";
 import { NavBar } from "../../components/NavBar";
 import { CardAtividade } from "../../components/CardAtividade";
 import { CardTurma } from "../../components/CardTurma";
+import { RectButton } from 'react-native-gesture-handler';
 
 import { useAuth } from "../../contexts/auth";
 
@@ -21,55 +22,23 @@ import api from "../../services/api";
 
 import ContentLoader, { Rect } from "react-content-loader/native";
 import { Avatar } from "../../components/Avatar";
-
-interface InfoAluno {
-  nomeTurma: string;
-}
-
+import { LabelIcon } from "../../components/LabelIcon";
 
 export function Configuracoes() {
   const { user } = useAuth();
-
-  const [info, setInfo] = useState<InfoAluno>();
-
+  
   const role = user?.role;
   const color = role === "ALUNO" ? theme.colors.green90 : theme.colors.purple90;
 
+  const [ editMode, setEditMode ] = useState(false);
   const [ loading, setLoading ] = useState(true);
 
-	useEffect(() => {
-		async function getInfoAluno() {
-			if (role === 'ALUNO') {
-				const {	
-					data,
-					status 
-				} = await api.get('/turmas/'); 
-
-				setInfo(data);
-			}
-			
-			setLoading(false);
-		}
-
-		getInfoAluno();
-	}, []);
-
-  /*useEffect(() => {
-		async function getInfo() {
-        const {	
-          data,
-          status 
-        } = user?.role === 'Aluno' ? await api.get('/Aluno/' + {user?.id}) : await api.get('/pagina-inicial');
-				setInfo(data);	
-    }
-
-		getInfo();
-	}, []);*/
+  const [ userState, setUserState ] = useState({ foto: user?.foto, email: user?.email, telefone: user?.telefone, nome: user?.nome, senha: ''});
 
   return (
     <View style={styles.container}>
       <NavBar title="Configurações" iconName="cog" color={color} />
-      <ScrollView style={styles.content}>
+      <ScrollView style={[styles.content]} >
         <View style={styles.avatarDiv}>
           <View
             style={[styles.avatarBackground, { backgroundColor: color }]}
@@ -88,29 +57,53 @@ export function Configuracoes() {
             ]}
           />
         </View>
-
         <View style={styles.userInfo}>
+                  
           <LabelText title="Apelido" color={color} />
-          <TextInput style={styles.textConfig}>{user?.nome}</TextInput>
+          <TextInput 
+          style={[styles.textConfig, editMode && styles.textEdit]} 
+          onChangeText={text => setUserState(previousState => ({ ...previousState, nome: text }))}
+          value={userState.nome}
+          
+          editable={editMode}
+          />
 
           <LabelText title="Email" color={color} />
-          <TextInput style={styles.textConfig}>{user?.email}</TextInput>
+          <TextInput 
+          style={[styles.textConfig, editMode && styles.textEdit]} 
+          onChangeText={text => setUserState(previousState => ({ ...previousState, email: text }))}
+          value={userState.email}
+          editable={editMode}
+          />
+
+          <LabelText title="Telefone" color={color} />
+          <TextInput 
+          style={[styles.textConfig, editMode && styles.textEdit]} 
+          onChangeText={text => setUserState(previousState => ({ ...previousState, telefone: text }))}
+          value={userState.telefone}
+          editable={editMode}
+          />
 
           <LabelText title="Senha" color={color} />
-          <TextInput style={styles.textConfig}>{"**********"}</TextInput>
-
-          {/*role === 'ALUNO'&& 
-          <View>
-            <LabelText title="Serie" color={color} />
-            <Text style={styles.textConfig}>{info?.serie</Text>
-          </View>
-            */}
-          
-
-          <LabelText title="Tema" color={color} />
-          <Text style={styles.textConfig}>Em breve</Text>
+          <TextInput 
+            style={[styles.textConfig, editMode && styles.textEdit]} 
+            secureTextEntry
+            onChangeText={text => setUserState(previousState => ({ ...previousState, senha: text }))}
+            value={editMode ? userState.senha : '**********'}
+            editable={editMode}
+          />
         </View>
       </ScrollView>
+      <View style={styles.content}>
+      <RectButton 
+          style={[styles.configButton, {backgroundColor: color}]}
+          onPress={() => {editMode ? setEditMode(false) : setEditMode(true)}}
+		    >
+
+			      <LabelIcon title={editMode? "Concluido" : "Editar"} iconName={"pen"}/>
+			
+		  </RectButton>
+      </View>
     </View>
   );
 }
--- src/screens/Configuracoes/styles.ts
+++ src/screens/Configuracoes/styles.ts
@@ -31,11 +31,28 @@ export const styles = StyleSheet.create({
   },
 
   textConfig: {
+    paddingLeft: 5,
+    marginTop: 2,
     color: theme.colors.white,
     fontFamily: theme.fonts.text400,
     fontSize: 20,
+    marginBottom: 20,
   },
 
+  configButton: {
+    width: '100%',
+    height: 60,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingRight: 10,
+    backgroundColor: theme.colors.gray90,
+    borderRadius: 10,
+    marginTop: 15,
+  },
 
+  textEdit: {
+    backgroundColor: theme.colors.gray70,
+    borderRadius: 10
+  }
 
 });
