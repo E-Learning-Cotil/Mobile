da6581dffe18562b3fba8c4a9202ac5bcf405c49
Gianni Labella <gianni0labella@gmail.com>
2021-11-11 00:07:40 -0300

    Update EmptyChat Component

--- src/components/EmptyChat/index.tsx
+++ src/components/EmptyChat/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
+
+import  BeginChat from '../../assets/beginChat.png';
 
 import { theme } from '../../global/styles/theme';
 import { styles } from './styles';
@@ -8,7 +10,13 @@ import { styles } from './styles';
 export function EmptyChat() {
 	return (
 		<View style={styles.container}>
-			<Text style={styles.text}>AAAAAAAAAAAAAA</Text>
+			<Image
+				source={BeginChat}
+				style={styles.image}
+				resizeMode={'contain'}
+			/>
+
+			<Text style={styles.text}>Começar a conversar</Text>
 		</View>
 	);
 }
--- src/components/EmptyChat/styles.ts
+++ src/components/EmptyChat/styles.ts
@@ -4,17 +4,28 @@ import { theme } from '../../global/styles/theme';
 
 export const styles = StyleSheet.create({
 	container: {
-		flex: 1,
+		height: Dimensions.get('window').height - 130,
 		padding: 20,
 
-		backgroundColor: 'green',
+		// backgroundColor: 'green',
 
 		flexDirection: 'column',
 		justifyContent: 'center',
 		alignItems: 'center',
+
+		transform: [{ scaleY: -1 }],
+	},
+
+	image: {
+		width: '100%',
+		height: 300,
+
+		// backgroundColor: 'red'
 	},
 
 	text: {
-		color: 'red',
+		fontFamily: theme.fonts.text700,
+		fontSize: 32,
+		color: theme.colors.white,
 	},
 });
