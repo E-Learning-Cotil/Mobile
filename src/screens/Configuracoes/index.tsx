dc0fdc0fb5386d81431ff8b2af33e63313df83a6
Gianni Labella <gianni0labella@gmail.com>
2021-11-15 18:38:16 -0300

    Update Configuracoes Screen
    Co-Authored-By: Matheus Mastelini  <60967298+Amazing512@users.noreply.github.com>

--- src/screens/Configuracoes/index.tsx
+++ src/screens/Configuracoes/index.tsx
@@ -1,17 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useRef } from "react";
 
 import {
-  View,
-  TextInput,
-  Text,
-  ActivityIndicator,
-  ScrollView,
+	View,
+	Text,
+	TextInput,
+	ScrollView,
 } from "react-native";
 
 import { LabelText } from "../../components/LabelText";
 import { NavBar } from "../../components/NavBar";
-import { CardAtividade } from "../../components/CardAtividade";
-import { CardTurma } from "../../components/CardTurma";
 import { RectButton } from 'react-native-gesture-handler';
 
 import { useAuth } from "../../contexts/auth";
@@ -20,90 +17,189 @@ import { theme } from "../../global/styles/theme";
 import { styles } from "./styles";
 import api from "../../services/api";
 
-import ContentLoader, { Rect } from "react-content-loader/native";
 import { Avatar } from "../../components/Avatar";
 import { LabelIcon } from "../../components/LabelIcon";
 
+import * as ImagePicker from 'expo-image-picker';
+
 export function Configuracoes() {
-  const { user } = useAuth();
-  
-  const role = user?.role;
-  const color = role === "ALUNO" ? theme.colors.green90 : theme.colors.purple90;
+	const { user, setUser } = useAuth();
 
-  const [ editMode, setEditMode ] = useState(false);
-  const [ loading, setLoading ] = useState(true);
+	const role = user?.role;
+	const color = role === "ALUNO" ? theme.colors.green90 : theme.colors.purple90;
 
-  const [ userState, setUserState ] = useState({ foto: user?.foto, email: user?.email, telefone: user?.telefone, nome: user?.nome, senha: ''});
+	const [editMode, setEditMode] = useState(false);
+	const [loading, setLoading] = useState(true);
 
-  return (
-    <View style={styles.container}>
-      <NavBar title="Configurações" iconName="cog" color={color} />
-      <ScrollView style={[styles.content]} >
-        <View style={styles.avatarDiv}>
-          <View
-            style={[styles.avatarBackground, { backgroundColor: color }]}
-          ></View>
-          <Avatar
-            urlImage={user?.foto}
-            imageBorderRadius={{ borderRadius: 10 }}
-            style={[
-              {
-                width: 100,
-                height: 100,
-                marginTop: 20,
-                marginLeft: 20,
-                position: "absolute",
-              },
-            ]}
-          />
-        </View>
-        <View style={styles.userInfo}>
-                  
-          <LabelText title="Apelido" color={color} />
-          <TextInput 
-          style={[styles.textConfig, editMode && styles.textEdit]} 
-          onChangeText={text => setUserState(previousState => ({ ...previousState, nome: text }))}
-          value={userState.nome}
-          
-          editable={editMode}
-          />
+	const scrollViewRef = useRef<ScrollView>(null);
+	const firstTextInputRef = useRef<TextInput>(null);
 
-          <LabelText title="Email" color={color} />
-          <TextInput 
-          style={[styles.textConfig, editMode && styles.textEdit]} 
-          onChangeText={text => setUserState(previousState => ({ ...previousState, email: text }))}
-          value={userState.email}
-          editable={editMode}
-          />
+	const [userState, setUserState] = useState({
+		foto: user?.foto || '',
+		email: user?.email || '',
+		telefone: user?.telefone || '',
+		nome: user?.nome || '',
+		senha: ''
+	});
 
-          <LabelText title="Telefone" color={color} />
-          <TextInput 
-          style={[styles.textConfig, editMode && styles.textEdit]} 
-          onChangeText={text => setUserState(previousState => ({ ...previousState, telefone: text }))}
-          value={userState.telefone}
-          editable={editMode}
-          />
+	async function handleChoosePhoto () {
+		const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!');
+        }
+		let result = await ImagePicker.launchImageLibraryAsync({
+			mediaTypes: ImagePicker.MediaTypeOptions.Images,
+			allowsEditing: true,
+			quality: 0.5,
+		});
 
-          <LabelText title="Senha" color={color} />
-          <TextInput 
-            style={[styles.textConfig, editMode && styles.textEdit]} 
-            secureTextEntry
-            onChangeText={text => setUserState(previousState => ({ ...previousState, senha: text }))}
-            value={editMode ? userState.senha : '**********'}
-            editable={editMode}
-          />
-        </View>
-      </ScrollView>
-      <View style={styles.content}>
-      <RectButton 
-          style={[styles.configButton, {backgroundColor: color}]}
-          onPress={() => {editMode ? setEditMode(false) : setEditMode(true)}}
-		    >
+		if (result.cancelled === false) {
+			const fileUri = result.uri;
+			const fileName = fileUri.substring(fileUri.lastIndexOf('/') + 1);
+			const fileExtension = fileName.split('.').pop();
 
-			      <LabelIcon title={editMode? "Concluido" : "Editar"} iconName={"pen"}/>
-			
-		  </RectButton>
-      </View>
-    </View>
-  );
+			let formData = new FormData();
+			formData.append('file', {
+				uri: fileUri,
+				name: fileName,
+				type: `image/${fileExtension}`
+			} as any);
+
+			try {
+				const {
+					data,
+					status
+				} = await api.post('/arquivos', formData, {
+					headers: {
+						'Content-Type': 'multipart/form-data',
+						'basic_token': process.env.BASIC_TOKEN,
+					},
+				});
+
+				setUserState(prevState => ({ ...prevState, foto: data.link }));
+			} catch (error) {
+				console.log(error)
+			}
+		}
+	}
+
+	async function configsUpdateHandler () {
+		if (user)
+		try {
+			const {
+				data,
+				status
+			} = await api.put('/alunos', {
+				foto: userState.foto !== user?.foto ? userState.foto : undefined,
+				nome: userState.nome !== user?.nome ? userState.nome : undefined,
+				email: userState.email !== user?.email ? userState.email : undefined,
+				telefone: userState.telefone !== user?.telefone ? userState.telefone : undefined,
+				senha: userState.senha,
+			});
+
+			setUser({ ...user, nome: userState.nome, email: userState.email, telefone: userState.telefone });
+		} catch (error) {
+			console.log(error);
+		}
+	}
+
+	return (
+		<View style={styles.container}>
+			<NavBar title="Configurações" iconName="cog" color={color} />
+			<ScrollView style={styles.content} ref={scrollViewRef} >
+				<View style={styles.avatarView}>
+					<View
+						style={[styles.avatarBackground, { backgroundColor: color }]}
+					></View>
+					<Avatar
+						urlImage={userState.foto}
+						imageBorderRadius={{ borderRadius: 10 }}
+						style={styles.avatar}
+					/>
+					<RectButton
+						style={[styles.avatarPicker, !editMode && { zIndex: -1 }]}
+						onPress={handleChoosePhoto}
+					>
+						<View style={styles.avatarPickerView}>
+							<Text style={styles.avatarPickerText}>
+								Alterar
+							</Text>
+						</View>
+					</RectButton>
+				</View>
+				<View style={styles.userInfo}>
+					<LabelText title="Apelido" color={color} />
+					<TextInput
+						style={[styles.textConfig, editMode && styles.textEdit]}
+						onChangeText={text => setUserState(previousState => ({ ...previousState, nome: text }))}
+						value={userState.nome}
+						editable={editMode}
+						ref={firstTextInputRef}
+						onFocus={() => {
+							scrollViewRef.current?.scrollTo({ y: 150, animated: true})
+						}}
+					/>
+
+					<LabelText title="Email" color={color} />
+					<TextInput
+						style={[styles.textConfig, editMode && styles.textEdit]}
+						onChangeText={text => setUserState(previousState => ({ ...previousState, email: text }))}
+						value={userState.email}
+						editable={editMode}
+						onFocus={() => {
+							scrollViewRef.current?.scrollTo({ y: 240, animated: true})
+						}}
+					/>
+
+					<LabelText title="Telefone" color={color} />
+					<TextInput
+						style={[styles.textConfig, editMode && styles.textEdit]}
+						onChangeText={text => setUserState(previousState => ({ ...previousState, telefone: text }))}
+						value={userState.telefone}
+						editable={editMode}
+						onFocus={() => {
+							scrollViewRef.current?.scrollTo({ y: 330, animated: true})
+						}}
+					/>
+
+					<LabelText title={editMode ? 'Nova Senha' : 'Senha'} color={color} />
+					<TextInput
+						style={[styles.textConfig, editMode && styles.textEdit, { marginBottom: 120 }]}
+						secureTextEntry
+						onChangeText={text => setUserState(previousState => ({ ...previousState, senha: text }))}
+						value={editMode ? userState.senha : '**********'}
+						editable={editMode}
+						spellCheck={false}
+						onFocus={() => {
+							scrollViewRef.current?.scrollTo({ y: 420, animated: true})
+						}}
+					/>
+				</View>
+			</ScrollView>
+			<View style={styles.buttonView}>
+				<RectButton
+					style={[styles.configButton, { backgroundColor: color }]}
+					onPress={() => {
+						if (editMode){
+							setEditMode(false);
+							configsUpdateHandler();
+						}
+						else {
+							setUserState({
+								foto: user?.foto || '',
+								email: user?.email || '',
+								telefone: user?.telefone || '',
+								nome: user?.nome || '',
+								senha: ''
+							});
+							setEditMode(true);
+							firstTextInputRef.current?.focus();
+						}
+					}}
+				>
+					<LabelIcon title={editMode ? "Concluído" : "Editar"} iconName={"pen"} />
+				</RectButton>
+			</View>
+		</View>
+	);
 }
--- src/screens/Configuracoes/styles.ts
+++ src/screens/Configuracoes/styles.ts
@@ -3,56 +3,104 @@ import { StyleSheet } from 'react-native';
 import { theme } from '../../global/styles/theme';
 
 export const styles = StyleSheet.create({
-  container: {
+	container: {
 		flex: 1,
 		paddingTop: 100,
 		backgroundColor: theme.colors.background,
-    //alignItems: 'center',
 	},
 
-  avatarDiv: {
-    height: 120,
-    width: '100%',
-  },
+	avatarView: {
+		height: 120,
+		width: '100%',
+	},
 
-  avatarBackground: {
-     height:100,
-     width: '100%',
-     borderRadius: 10,
-    },
+	avatarBackground: {
+		height: 100,
+		width: '100%',
+		borderRadius: 10,
+	},
 
-  content: {
+	avatar: {
+		width: 100,
+		height: 100,
+		marginTop: 20,
+		marginLeft: 20,
+		position: "absolute",
+	},
+
+	avatarPicker: {
+		width: 100,
+		height: 100,
+		marginTop: 20,
+		marginLeft: 20,
+		position: "absolute",
+	},
+
+	avatarPickerView: {
+		width: '100%',
+		height: '100%',
+
+		borderColor: theme.colors.white,
+		borderWidth: 2,
+		borderRadius: 10,
+
+		backgroundColor: '#00000066',
+
+		justifyContent: 'center',
+		alignItems: 'center',
+		textAlign: 'center',
+		textAlignVertical: 'center',
+	},
+
+	avatarPickerText: {
+		fontFamily: theme.fonts.text400,
+		fontSize: 24,
+		color: theme.colors.white,
+	},
+
+	content: {
 		padding: 20,
 		paddingTop: 20,
 	},
 
-  userInfo: {
-    marginTop: 10,
-  },
+	userInfo: {
+		marginTop: 10,
+	},
 
-  textConfig: {
-    paddingLeft: 5,
-    marginTop: 2,
-    color: theme.colors.white,
-    fontFamily: theme.fonts.text400,
-    fontSize: 20,
-    marginBottom: 20,
-  },
+	textConfig: {
+		left: -10,
+		padding: 0,
+		paddingLeft: 12,
+		marginTop: 2,
+		color: theme.colors.white,
+		fontFamily: theme.fonts.text400,
+		fontSize: 20,
+		marginBottom: 20,
+	},
 
-  configButton: {
-    width: '100%',
-    height: 60,
-    alignItems: 'center',
-    justifyContent: 'center',
-    paddingRight: 10,
-    backgroundColor: theme.colors.gray90,
-    borderRadius: 10,
-    marginTop: 15,
-  },
+	textEdit: {
+		padding: 0,
+		borderBottomColor: theme.colors.white,
+		borderBottomWidth: 0.8,
+		borderRadius: 10,
+	},
 
-  textEdit: {
-    backgroundColor: theme.colors.gray70,
-    borderRadius: 10
-  }
+	buttonView: {
+		position: 'absolute',
+		right: 12,
+		bottom: 20,
+		left: 12,
+	},
 
+	configButton: {
+		width: '100%',
+		height: 60,
+		paddingRight: 10,
+
+		alignItems: 'center',
+		justifyContent: 'center',
+
+		backgroundColor: theme.colors.gray90,
+		borderRadius: 10,
+	},
 });
