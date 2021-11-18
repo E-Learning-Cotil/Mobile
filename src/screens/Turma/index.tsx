a50be5f25513124d0c50ed298276c00e749bc258
Gianni Labella <gianni0labella@gmail.com>
2021-11-18 11:12:54 -0300

    Update Turma Screen - Add Topic Creation
    Co-Authored-By: Matheus Mastelini  <60967298+Amazing512@users.noreply.github.com>

--- src/screens/Turma/index.tsx
+++ src/screens/Turma/index.tsx
@@ -1,21 +1,20 @@
 import React, { useEffect, useState } from 'react';
 
-import { View, ScrollView, Text } from 'react-native';
+import { View, ScrollView, TouchableOpacity, Text, Modal, TextInput, ActivityIndicator } from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons'; 
 
-import { LabelText } from '../../components/LabelText';
 import { NavBar } from '../../components/NavBar';
-import { CardAtividade } from '../../components/CardAtividade';
-import { CardTurma } from '../../components/CardTurma';
+import { CardTopico } from '../../components/CardTopico';
 
 import { useAuth } from '../../contexts/auth';
+import api from '../../services/api';
 
 import { theme } from '../../global/styles/theme';
 import { styles } from './styles';
-import api from '../../services/api';
-import { CardTopico } from '../../components/CardTopico';
 
 
 interface DadosTurma {
+	id: number;
 	nome: string;
 	cores: {
 		corPrim: string;
@@ -30,101 +29,193 @@ interface Topicos {
 	descricao: string;
 }
 
-export function Turma({ route, navigation }: any){
+export function Turma({ route, navigation }: any) {
 	const { id } = route.params;
 	const { user } = useAuth();
 	const role = user?.role;
 
-	
-	
 	const [ loading, setLoading ] = useState(true);
-	
+
+	const [ creating, setCreating ] = useState(false);
+	const [ modalVisible, setModalVisible ] = useState(false);
+	const [ newTopicTitle, setNewTopicTitle ] = useState('');
+	const [ newTopicDescription, setNewTopicDescription ] = useState('');
+
 	const [ dados, setDados ] = useState<DadosTurma>();
-	const [topicos, setTopicos] = useState<Topicos[]>([]);
-	
-	useEffect(() => {		
-		async function getDados() {
-			try{
-				const {
-					data,	
-					status
-				} = await api.get(`/turmas/${id}`);
-				setDados(data);
-			} catch (error: any) {
-				console.log('Error Turma: ', error.response.data.error);
-			}	
-			
+	const [ topicos, setTopicos ] = useState<Topicos[]>([]);
+
+	async function createNewTopicHandler () {
+		if (newTopicTitle !== '' && newTopicDescription !== '') {
+			try {
+				setCreating(true);
+
+				await api.post('/topicos', {
+					idTurma: dados?.id,
+					nome: newTopicTitle,
+					descricao: newTopicDescription,
+				});
+				
+				await load();
+				
+				setNewTopicTitle('');
+				setNewTopicDescription('');
+				setCreating(false);
+				setModalVisible(false);
+			} catch (error) {
+				console.error(error);
+			}
+		}
+	}
+
+	async function getDados() {
+		try {
+			const {
+				data,
+				status
+			} = await api.get(`/turmas/${id}`);
+			setDados(data);
+		} catch (error: any) {
+			console.log('Error Turma: ', error.response.data.error);
 		}
 
-		async function getTopicos() {
-			try{
-				const {
-					data,	
-					status
-				} = await api.get(`/topicos/?idTurma=${id}`);
+	}
 
-				setTopicos(data);
-			} catch (error: any) {
-				console.log('Error Turma: ', error.response.data.error);
-			}	
-		}
+	async function getTopicos() {
+		try {
+			const {
+				data,
+				status
+			} = await api.get(`/topicos/?idTurma=${id}`);
 
-		async function load(){
-			await getDados();
-			await getTopicos();
-			setLoading(false);
+			setTopicos(data);
+		} catch (error: any) {
+			console.log('Error Turma: ', error.response.data.error);
 		}
+	}
+
+	async function load() {
+		await getDados();
+		await getTopicos();
+		setLoading(false);
+	}
+
+	useEffect(() => {
 		setLoading(true);
 		load();
 	}, [id]);
 
-	if(!loading){
-		return( 
+	if (!loading) {
+		return (
 			<View style={[styles.container]}>
-					<NavBar 
-						title={ dados?.nome } 
-						
-						iconName={ dados?.icone.altLink }
-							color={ dados?.cores.corPrim }
-						/>
+				<NavBar
+					title={dados?.nome}
 
-						<ScrollView style={styles.content}>
-							<View style={styles.topicosList}>
+					iconName={dados?.icone.altLink}
+					color={dados?.cores.corPrim}
+				/>
+
+				<ScrollView style={styles.content}>
+					<View style={styles.topicosList}>
+						{
+							(topicos).map(topico => {
+								const id = topico.id;
+								const title = topico.nome;
+								const descricao = topico.descricao;
+
+								return <CardTopico
+									key={id}
+									id={id}
+									title={title}
+									description={descricao}
+								/>
+							})
+						}
+					</View>
+				</ScrollView>
+
+				{
+					role === 'PROFESSOR' &&
+					<View style={styles.buttonView}>
+						<TouchableOpacity
+							style={[styles.createTopicoButton, { backgroundColor: dados?.cores.corPrim }]}
+							onPress={ () => setModalVisible(true) }
+						>
+							<Text style={styles.createTopicoButtonText}>
+								Tópico
+							</Text>
+							<FontAwesome5 name="plus" size={24} color="white" />
+						</TouchableOpacity>
+					</View>
+				}
+
+				<Modal
+					animationType="slide"
+					transparent={true}
+					visible={modalVisible}
+					onRequestClose={() => {
+						setModalVisible(!modalVisible);
+					}}>
+					<View style={styles.modal}>
+						<View style={styles.modalContent}>
+							<Text style={styles.modalText}>
+								Nome do tópico:
+							</Text>
+							<TextInput
+								style={styles.modalTextInput}
+								onChangeText={setNewTopicTitle}
+								value={newTopicTitle}
+								placeholder={'(Obrigatório)'}
+								placeholderTextColor={theme.colors.gray70}
+							/>
+							<Text style={styles.modalText}>
+								Descrição do tópico:
+							</Text>
+							<TextInput
+								style={{...styles.modalTextInput, ...{ height: 90 }}}
+								onChangeText={setNewTopicDescription}
+								value={newTopicDescription}
+								multiline={true}
+								placeholder={'(Obrigatório)'}
+								placeholderTextColor={theme.colors.gray70}
+							/>
+
+							<TouchableOpacity
+								style={[styles.modalButton, { backgroundColor: dados?.cores.corPrim }]}
+								onPress={ createNewTopicHandler }
+							>
 								{
-									(topicos).map(topico => {
-										const id = topico.id;
-										const title = topico.nome;
-										const descricao = topico.descricao;
-		
-										return <CardTopico 
-											key={ id } 
-											id={ id }
-											title={ title } 
-											description={ descricao } 
-										/>
-									})
+									creating ?
+									<ActivityIndicator
+										size="large"
+										color="#fff"
+										animating={true}
+									/> :
+									<Text style={styles.modalButtonText}>
+										Criar
+									</Text>
 								}
-							</View>
-						</ScrollView>
+							</TouchableOpacity>
+						</View>
+					</View>
+				</Modal>
 			</View>
 		);
 	}
-	else
-	{
-		return(
+	else {
+		return (
 			<View style={[styles.container]}>
-				<NavBar color={theme.colors.highlight}/>
+				<NavBar color={theme.colors.highlight} />
 				<ScrollView style={styles.content} scrollEnabled={false}>
-				{
-					[...Array(6)].map((value, index) => {
-						return <CardTopico 
-							key={index} 
-							loading={true} 
-						/>
-					})
-				}
+					{
+						[...Array(6)].map((value, index) => {
+							return <CardTopico
+								key={index}
+								loading={true}
+							/>
+						})
+					}
 				</ScrollView>
 			</View>
 		);
-	}			
+	}
 }
--- src/screens/Turma/styles.ts
+++ src/screens/Turma/styles.ts
@@ -15,5 +15,116 @@ export const styles = StyleSheet.create({
 
 	topicosList: {
 		paddingTop: 5,
+		marginBottom: 30,
+	},
+
+	buttonView: {
+		flexDirection: 'row',
+		justifyContent: 'center',
+
+		paddingHorizontal: 15,
+		paddingVertical: 15,
+		backgroundColor: theme.colors.gray90,
+
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 12,
+		},
+		shadowOpacity: 0.58,
+		shadowRadius: 16.00,
+
+		elevation: 24,
+	},
+
+	createTopicoButton: {
+		flex: 1,
+
+		height: 45,
+
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+
+		paddingHorizontal: 20,
+
+		borderRadius: 4,
+	},
+
+	createTopicoButtonText: {
+		fontFamily: theme.fonts.text700,
+		fontSize: 24,
+		lineHeight: 26,
+		color: theme.colors.white,
+
+		textAlign: 'center',
+		textAlignVertical: 'center',
+	},
+
+	modal: {
+		position: 'absolute',
+		top: 0,
+		right: 0,
+		bottom: 0,
+		left: 0,
+
+		backgroundColor: `${theme.colors.black}55`,
+
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+
+	modalContent: {
+		width: '80%',
+
+		backgroundColor: theme.colors.gray90,
+		paddingHorizontal: 15,
+		paddingVertical: 20,
+
+		borderRadius: 10,
+
+		flexDirection: 'column',
+	},
+
+	modalText: {
+		fontFamily: theme.fonts.text700,
+		fontSize: 18,
+		color: theme.colors.white,
+
+		marginBottom: 5,
+	},
+
+	modalTextInput: {
+		paddingHorizontal: 10,
+
+		fontFamily: theme.fonts.text700,
+		fontSize: 16,
+		color: theme.colors.white,
+		textAlignVertical: 'top',
+
+		backgroundColor: theme.colors.black,
+		borderRadius: 5,
+
+		marginBottom: 10,
+	},
+
+	modalButton: {
+		height: 40,
+
+		flexDirection: 'row',
+		justifyContent: 'center',
+		alignItems: 'center',
+
+		borderRadius: 4,
+	},
+
+	modalButtonText: {
+		fontFamily: theme.fonts.text700,
+		fontSize: 20,
+		lineHeight: 22,
+		color: theme.colors.white,
+
+		textAlign: 'center',
+		textAlignVertical: 'center',
 	},
 });
